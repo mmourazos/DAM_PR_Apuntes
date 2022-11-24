454472f2819a61397f3688c5a18d7554409799ac
mmourazos <mourazos@edu.xunta.gal>
2022-11-24 13:39:59 +0100

    Añado apuntes y ejemplos de javadoc

---
+++ docs/Tema 2/codigo_fuente/documentacion/src/mipaquete/Clase.java
@@ -0,0 +1,55 @@
+package mipaquete;
+
+/**
+ * Esta clase es la clase principal del paquete paquete.
+ */
+public class Clase {
+    /**
+     * Ejemplo de un atributo de esta clase.
+     */
+    private int propiedad;
+
+    /**
+     * Constructor por defecto de la clase.
+     */
+    public Clase() {
+        this.propiedad = 0;
+    }
+
+    /**
+     * Constructor parametrizado de la clase.
+     * 
+     * @param propiedad Valor del atributo de la clase.
+     */
+    public Clase(int propiedad) {
+        this.propiedad = propiedad;
+    }
+
+    /**
+     * Método que devuelve el valor del atributo de la clase.
+     * 
+     * @return Valor del atributo de la clase.
+     * @deprecated Este método está obsoleto. Utilice el método getPropiedad().
+     */
+    public int dimePropiedad() {
+        return propiedad;
+    }
+
+    /**
+     * Método que devuelve el valor del atributo de la clase.
+     * 
+     * @return Valor del atributo de la clase.
+     */
+    public int getPropiedad() {
+        return propiedad;
+    }
+
+    /**
+     * Método que modifica el valor del atributo de la clase.
+     * 
+     * @param propiedad Nuevo valor del atributo de la clase.
+     */
+    public void setPropiedad(int propiedad) {
+        this.propiedad = propiedad;
+    }
+}
