a3999f7f448219d515e83b64e676f25f3dffdca1
mmourazos <mourazos@edu.xunta.gal>
2023-03-11 17:59:49 +0100

    Probando docfx

--- docs/Tema 11 - GUI/codigo_fuente/ReservasMVVM/ReservasMVVM/Modelo/IDHabitacion.cs
+++ docs/Tema 11 - GUI/codigo_fuente/ReservasMVVM/ReservasMVVM/Modelo/IDHabitacion.cs
@@ -37,9 +37,9 @@ namespace ReservasMVVM.Modelo
 
         public static bool operator ==(IDHabitacion id1, IDHabitacion id2)
         {
-            if (id1 == null && id2 == null) return true;
+            if (id1 is null && id2 is null) return true;
 
-            return id1 != null && id1.Equals(id2);
+            return !(id1 is null && id1.Equals(id2));
         }
 
         public static bool operator !=(IDHabitacion id1, IDHabitacion id2)
